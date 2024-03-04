5a2116909d14b97bbc9d426c1df3c65cebdfbf4f
Leah Neukirchen <leah@vuxu.org>
2024-03-04 20:44:31 +0100

    build-alpine.sh: use podman build.

--- build-alpine.sh
+++ build-alpine.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 rm -rf bin.alpine
-docker build -f Dockerfile.alpine --progress=plain --iidfile=id.alpine .
-docker save $(cat id.alpine) | bsdtar xO '*/layer.tar' | bsdtar xv --ignore-zeros bin.alpine/nitro bin.alpine/nitroctl
+mkdir bin.alpine
+podman build -f Containerfile.alpine -v $PWD/bin.alpine:/out:Z
