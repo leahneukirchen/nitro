0e4b7f60aad27ba65c5e78ec3fd7ae2e6d3466b0
Leah Neukirchen <leah@vuxu.org>
2025-08-19 17:43:01 +0200

    nitro_exporter.rb: don't compute HOSTNAME.dump all the time

--- contrib/nitro_exporter.rb
+++ contrib/nitro_exporter.rb
@@ -6,6 +6,7 @@ require 'rack/handler/thin'
 
 class NitroExporter
   HOSTNAME = `hostname -f`.strip
+  HOSTNAME_DUMP = HOSTNAME.dump
 
   def call(env)
     r = Rack::Response.new
@@ -13,14 +14,14 @@ class NitroExporter
 
     state = `nitroctl`
     if $?.exitstatus != 0
-      r.write %Q|nitro_up{hostname=#{HOSTNAME.dump}} 0\n|
+      r.write %Q|nitro_up{hostname=#{HOSTNAME_DUMP}} 0\n|
     else
-      r.write %Q|nitro_up{hostname=#{HOSTNAME.dump}} 1\n|
+      r.write %Q|nitro_up{hostname=#{HOSTNAME_DUMP}} 1\n|
       state.each_line { |line|
         case line
         when /^([A-Z]+) (.*?) (\(pid \d+\) )?\(wstatus (\d+)\) (\d+)s$/
-          r.write %Q|nitro_state{hostname=#{HOSTNAME.dump},service=#{$2.dump},state=#{$1.dump}} #{$5}\n|
-          r.write %Q|nitro_wstatus{hostname=#{HOSTNAME.dump},service=#{$2.dump}} #{$4}\n|  if $4
+          r.write %Q|nitro_state{hostname=#{HOSTNAME_DUMP},service=#{$2.dump},state=#{$1.dump}} #{$5}\n|
+          r.write %Q|nitro_wstatus{hostname=#{HOSTNAME_DUMP},service=#{$2.dump}} #{$4}\n|  if $4
         end
       }
 
